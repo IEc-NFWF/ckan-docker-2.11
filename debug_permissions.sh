2a8b28cb66282a3b002df92cad0dd95410b0fad3
Alex Kruger <akruger@indecon.com>
2025-03-22 21:21:35 -0400

    adjusted helper scripts slightly

--- debug_permissions.sh
+++ debug_permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec -u root -it ckan-docker-ckan-dev-1 chown -R ckan /srv/app/src_extensions/ckanext-nfwf_theme/
-docker exec -u root -it ckan-docker-ckan-dev-1 chown -R ckan /srv/app/src_extensions/ckanext-nfwf_fields/
+docker exec -u root -it ckan-docker-211-ckan-dev-1 chown -R ckan /srv/app/src_extensions/ckanext-nfwf_theme/
+docker exec -u root -it ckan-docker-211-ckan-dev-1 chown -R ckan /srv/app/src_extensions/ckanext-nfwf_fields/
--- restore_db.sh
+++ restore_db.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Check if an argument was provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <path_to_ckan_dump_file>"
-    echo "Example: $0 ./ckan.dump"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <path_to_ckan_dump_file> <path to contents of _data in ckan storage volume>"
+    echo "Example: $0 ./ckan.dump ./docker_ckan_storage/_data/"
     exit 1
 fi
 
 # Store the dump file path
 DUMP_FILE="$1"
+STORAGE_VOLUME="$2"
 
 # Check if the file exists
 if [ ! -f "$DUMP_FILE" ]; then
@@ -35,6 +36,15 @@ else
     echo "Database restore completed successfully."
 fi
 
+echo "Restoring the datastore from '$STORAGE_VOLUME'"
+sudo rsync -Pavvr "$STORAGE_VOLUME" $(docker volume inspect ckan-docker-211_ckan_storage | jq -r -c '.[] | .Mountpoint')
+sudo chown -R 503:502 /var/lib/docker/volumes/ckan-docker-211_ckan_storage
+
+echo "Setting datastore permissions..."
+docker exec ckan-docker-211-ckan-dev-1 ckan -c /srv/app/ckan.ini datastore set-permissions | docker exec -i ckan-docker-211-db-1 psql -U postgres --set ON_ERROR_STOP=1
+
+echo "Rebuilding search index..."
 docker exec -it ckan-docker-211-ckan-dev-1 ckan -c /srv/app/ckan.ini search-index rebuild
 
-echo "Database restore process finished."
+echo "Database restore process finished. Restarting the CKAN dev container..."
+bin/restart
